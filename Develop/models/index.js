18a36be7bef9a7f608b7386870f51eb07ece0f62
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-11 19:42:05 -0400

    finished up index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -10,11 +10,22 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
-// Categories have many Products
+// A Category can have multiple Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// A Product can have multiple Tags
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// A Tag can belong to multiple Products
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
