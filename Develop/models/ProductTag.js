23320834c67827085519398bfcf849e398e63ea0
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-11 19:44:36 -0400

    tags commit

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
+// Define the ProductTag model extending Sequelize's Model class
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+// Initialize ProductTag model with column definitions and configurations
+ProductTag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product', // References the 'product' model
+      key: 'id', // Specifies the 'id' column in the 'product' model
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag', // References the 'tag' model
+      key: 'id', // Specifies the 'id' column in the 'tag' model
+    },
+  },
+}, {
+  sequelize, // Database connection instance
+  timestamps: false, // Disables automatic timestamp fields
+  freezeTableName: true, // Prevents Sequelize from renaming the table
+  underscored: true, // Uses underscores instead of camelCasing for column names
+  modelName: 'product_tag', // Sets the model name
+});
 
 module.exports = ProductTag;
