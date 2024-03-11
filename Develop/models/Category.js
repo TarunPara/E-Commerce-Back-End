298f16f7305a99ed3671be789e2b2d27327bb7a9
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-11 19:41:07 -0400

    Category

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -4,5 +4,27 @@ const sequelize = require('../config/connection.js');
 // Define Category model
 class Category extends Model {}
 
+// Initialize Category model with its columns and configuration
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false, // Disable timestamps for this model
+    freezeTableName: true, // Prevent sequelize from renaming the table
+    underscored: true, // Use underscores instead of camel-casing
+    modelName: 'category', // Explicitly define model name
+  }
+);
 
 module.exports = Category;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,11 +1,15 @@
-// import models
+// Importing model classes
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Setting up model associations
 
+// A Product belongs to a single Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
